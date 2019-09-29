985434527710f514fbbf22cc8abd9e1546f47b43
jakasco <kasperi.mutku@elisanet.fi>
2019-09-29 19:41:24 +0300

    Html video player toimii nyt Reactissa

---
+++ kissa-sivy/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Page from './custom_videoplayer/video.html';
+import style from './custom_videoplayer/Main.css';
+
+var htmlDoc = { __html: Page };
+
+class App extends Component {
+
+    componentDidMount() {
+
+        const script = document.createElement("script");
+        const jquery = document.createElement("script");
+
+        script.src = "/Js/main.js";
+        jquery.src = "/Js/jquery-1.10.2.min.js";
+
+        script.async = true;
+        jquery.async = true;
+
+        document.body.appendChild(script);
+        document.body.appendChild(jquery);
+    }
+
+    render(){
+        return (
+            <React.Fragment>
+                <div style={style} dangerouslySetInnerHTML={htmlDoc} />
+
+            </React.Fragment>
+        );
+    }
+}
+
+export default App;
